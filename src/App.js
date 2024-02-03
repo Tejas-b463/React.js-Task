6b4e75feffd5892b4cee0dae9a966ba8e2b3aa3c
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2024-02-03 23:25:12 +0530

    fetch api - Forex Rate API

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import FetchApi from "./components/FetchApi";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Forex Rate API</h1>
+      <FetchApi />
+    </div>
+  );
+}
+
+export default App;
